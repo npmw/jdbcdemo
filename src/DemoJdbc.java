edebd8bdcb0e77d98225bd88a7cbe10a9edf0a30
hxyj93 <fathimaashik2042@gmail.com>
2025-07-12 11:36:10 +0530

    2nd commit

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -1,8 +1,17 @@
 import java.sql.*;
 
 public class DemoJdbc {
-    public static void main(String[] args){
+    public static <Resultset> void main(String[] args) throws Exception{
 
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/demo","postgres","postgrenew123");
+            System.out.println("Connection established successfully");
 
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("Select * from studentdemo");
+            while(rs.next()){
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            conn.close();
     }
 }
