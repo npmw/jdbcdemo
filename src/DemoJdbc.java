ec4d03527e60a40d9d7ec598deaa11a9fc2568bd
hxyj93 <fathimaashik2042@gmail.com>
2025-07-12 12:21:18 +0530

    2nd commit

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -8,9 +8,17 @@ public class DemoJdbc {
             System.out.println("Connection established successfully");
 
             Statement stmt = conn.createStatement();
+
             ResultSet rs = stmt.executeQuery("Select * from studentdemo");
             while(rs.next()){
                 System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            String sql = "INSERT INTO studentdemo (sid, sname, qualified) VALUES (103, 'John Doe', false)";
+            if(stmt.executeUpdate(sql)==1){
+                System.out.println("Record inserted successfully");
+            } else {
+                System.out.println("Record insertion failed");
+
             }
             conn.close();
     }
